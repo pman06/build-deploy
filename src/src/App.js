1ea4b2ee9a8371f2bf470af890aaa74a824080f1
Paul <freemeonline@gmail.com>
2023-08-08 18:02:33 -0500

    Added test in index.js

--- src/src/App.js
+++ src/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
+        <p>Hello World, testing multi template in ADO!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +15,7 @@ function App() {
         >
           Learn React
         </a>
-		Welcome to my dev environments process.env.REACT_APP_SENTRY_URL
+        Welcome to my dev environments process.env.REACT_APP_SENTRY_URL
       </header>
     </div>
   );

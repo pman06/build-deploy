53e993e57156ce8f656e4659fc333d14ef31d962
Paul <freemeonline@gmail.com>
2023-08-13 17:10:26 -0500

    Added script for python

--- azure-pipeline/scripts/updateWI.py
+++ azure-pipeline/scripts/updateWI.py
@@ -24,7 +24,7 @@ print(response.json())
 output=response.json()
 count = output['count']
 def get_item(id):
-    fields=["System.WorkItemType","System.State"]
+    fields=["/fields/System.WorkItemType","/fields/System.State"]
     url=f'https://dev.azure.com/{organization}/{project}/_apis/wit/workitems/{id}?fields={fields}api-version=7.0'
     response=requests.get(url, headers=headers)
     print(response.json())

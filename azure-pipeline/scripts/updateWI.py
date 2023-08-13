b010e6a5d2de537f7415613b5b2e991cba9bed93
Paul <freemeonline@gmail.com>
2023-08-13 09:28:16 -0500

    Added script for python

--- azure-pipeline/scripts/updateWI.py
+++ azure-pipeline/scripts/updateWI.py
@@ -10,5 +10,18 @@
 # curl()
 
 import os
+import requests
+
 print(os.environ)
 print(os.environ['SYSTEM_ACCESSTOKEN'])
+print(os.environ['BUILD_BUILDID'])
+
+buildId=os.environ['BUILD_BUILDID']
+token=os.environ['SYSTEM_ACCESSTOKEN']
+project=os.environ['SYSTEM_TEAMPROJECT']
+organization='cooclass'
+Authorization=
+url=f'https://dev.azure.com/{organization}/{project}/_apis/build/builds/{buildId}/workitems?api-version=7.0'
+
+response = requests.get(api_url, headers=headers)
+print(response.json())
